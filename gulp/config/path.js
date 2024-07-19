4cdd697fc1051d279d2a6b42c42e207d786a6a89
Shipy4kaRU <moloko2004goda@gmail.com>
2024-07-19 20:29:17 +0400

    configure local server

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,24 +1,24 @@
 //Получаем имя папки проекта
-import * as nodePath from 'path'
+import * as nodePath from "path";
 export const rootFolder = nodePath.basename(nodePath.resolve());
 
 //Путь исходных файлов и путь результирующих файлов
-export const distFolder = 'dist';
-export const srcFolder = 'src';
+export const distFolder = "dist";
+export const srcFolder = "src";
 export const path = {
-    dist: {
-        html: `${distFolder}/`,
-        styles: `${distFolder}/css/`,
-        scripts: `${distFolder}/js/`,
-        images: `${distFolder}/img/`,
-        fonts: `${distFolder}/fonts/`
-    },
-    src: {
-        html: `${srcFolder}/*.html`,
-        styles: `${srcFolder}/styles/styles.scss`,
-        normilize: `${srcFolder}/styles/normalize.css`,
-        scripts: `${srcFolder}/scripts/**/*.js`,
-        images: `${srcFolder}/img/**/*.{jpg,png,svg,gif,ico,webp}`,
-        fonts: `${srcFolder}/fonts/`
-    }
-}
+  dist: {
+    html: `${distFolder}/`,
+    styles: `${distFolder}/css/`,
+    scripts: `${distFolder}/js/`,
+    images: `${distFolder}/img/`,
+    fonts: `${distFolder}/fonts/`,
+  },
+  src: {
+    html: `${srcFolder}/*.html`,
+    styles: `${srcFolder}/styles/styles.scss`,
+    normilize: `${srcFolder}/styles/normalize.css`,
+    scripts: `${srcFolder}/scripts/**/*.js`,
+    images: `${srcFolder}/img/**/*.{jpg,png,svg,gif,ico,webp}`,
+    fonts: `${srcFolder}/fonts/`,
+  },
+};
