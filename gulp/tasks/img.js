106e92f59a692109322bccc3581967136b5c38bf
Shipy4kaRU <moloko2004goda@gmail.com>
2024-07-20 23:31:23 +0400

    update img() function

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,15 +1,20 @@
-import newer from 'gulp-newer'
-import imagemin from 'gulp-imagemin'
-import size from 'gulp-size'
-import flatten from 'gulp-flatten';
+import newer from "gulp-newer";
+import imagemin, { gifsicle, mozjpeg, optipng, svgo } from "gulp-imagemin";
+import size from "gulp-size";
+import flatten from "gulp-flatten";
 
-export function img(){
-  return app.gulp.src(`${app.path.src.images}`, {encoding: false})
-  .pipe(newer(app.path.dist.images))
-  .pipe(imagemin({
-    progressive: true
-  }))
-  .pipe(size())
-  .pipe(flatten())
-  .pipe (app.gulp.dest(app.path.dist.images))
+export function img() {
+  return app.gulp
+    .src(`${app.path.src.images}`, { encoding: false })
+    .pipe(newer(app.path.dist.images))
+    .pipe(
+      imagemin([
+        gifsicle({ interlaced: true }),
+        mozjpeg({ quality: 75, progressive: true }),
+        optipng({ optimizationLevel: 3 }),
+      ])
+    )
+    .pipe(size())
+    .pipe(flatten())
+    .pipe(app.gulp.dest(app.path.dist.images));
 }
