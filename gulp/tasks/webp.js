f8e9d3a75c4fcf6af0b3991f3b582fb51ee9abfe
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-10-18 14:39:39 +0400

    add clean source for webp pictures

--- gulp/tasks/webp.js
+++ gulp/tasks/webp.js
@@ -1,8 +1,10 @@
 import webp from "gulp-webp";
+import flatten from "gulp-flatten";
 
 export function webpRun() {
   return app.gulp
     .src(`${app.path.src.imagesWebp}`, { encoding: false })
     .pipe(webp())
+    .pipe(flatten())
     .pipe(app.gulp.dest(app.path.dist.images));
 }
